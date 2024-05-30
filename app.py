393eb82c88b896d55a766a5f719cfda84652c3c3
Joe Schwab <jschwab21@users.noreply.huggingface.co>
2024-05-30 16:39:18 +0000

    Update app.py

--- app.py
+++ app.py
@@ -22,13 +22,13 @@ h2 {
     color: #eb5726;
 }
 h3 {
-    color: #eb5726;
+    color: #ffffff;
 } 
 h4 {
     color: #eb5726;
 } 
 h5 {
-    color: #eb5726;
+    color: #282828;
 }
 h6 {
     color: #eb5726; 
@@ -37,20 +37,15 @@ h6 {
     background-color: #eb5726;
     color: #ffffff;
     border: 2px solid #ecf0f1;
-    label: #ffffff
 }
-label[for^="video_url"] {
-    color: #ffffff;
-}
-#description{
+#description {
     background-color: #ecf0f1;
-    color: #f0531f;
+    color: #eb5726;
     border: 2px solid #eb5726;
 }
-
 #submit_button {
     background-color: #ffffff;
-    color: #eb5726;
+    color: #F06230;
     border: 2px solid #eb5726;
 }
 #submit_button:hover {
@@ -66,23 +61,23 @@ label[for^="video_url"] {
     text-align: center;
     width: 100%;
 }
-.textbox-label {
-    color: #eb5726;
-}
-#description ~ label {
-    color: #eb5726;
-}
+"""
+
+js = """
+document.querySelector('label[for="video_url"]').style.color = '#ffffff';
+document.querySelector('label[for="description"]').style.color = '#eb5726';
 """
 
 with gr.Blocks(css=css) as demo:
     with gr.Column():
         gr.Markdown("# Sickstadium AI", elem_classes="centered-markdown")
-        gr.Markdown("### Upload your video. Find sick clips. Tell your truth.", elem_classes="centered-markdown")
+        gr.Markdown("### This is a brief description for the webpage.", elem_classes="centered-markdown")
         video_url = gr.Textbox(label="Video URL or Filepath", elem_id="video_url")
-        description = gr.Textbox(label="## Description of desired clip", elem_id="centered-markdown")
+        description = gr.Textbox(label="Description of desired clip", elem_id="description")
         submit_button = gr.Button("Process Video", elem_id="submit_button")
         video_output = gr.Video(label="Processed Video", elem_id="video_output")
         download_output = gr.File(label="Download Processed Video", elem_id="download_output")
         submit_button.click(fn=display_results, inputs=[video_url, description], outputs=[video_output, download_output])
+        gr.HTML(f"<script>{js}</script>")
 
 demo.launch()
