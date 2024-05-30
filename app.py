5d5059e8bab978536d782532d89bc43b501621b9
Joe Schwab <jschwab21@users.noreply.huggingface.co>
2024-05-30 05:37:12 +0000

    Update app.py

--- app.py
+++ app.py
@@ -14,23 +14,39 @@ body {
     color: #ffffff;
     font-family: 'Arial', sans-serif;
 }
-h1, h2, h3, h4, h5, h6 {
+h1 {
     color: #282828;
+    text-align: center;
+}
+h2 {
+    color: #eb5726;
+}
+h3 {
+    color: #ffffff;
+} 
+h4 {
+    color: #eb5726;
+} 
+h5 {
+    color: #282828;
+}
+h6 {
+    color: #eb5726; 
 }
 #video_url {
     background-color: #eb5726;
     color: #ecf0f1;
-    border: 1px solid #ecf0f1;
+    border: 2px solid #ecf0f1;
 }
 #description {
     background-color: #ecf0f1;
     color: #eb5726;
-    border: 1px solid #eb5726;
+    border: 2px solid #eb5726;
 }
 #submit_button {
     background-color: #ffffff;
     color: #F06230;
-    border: 1px solid #F06230;
+    border: 2px solid #eb5726;
 }
 #submit_button:hover {
     background-color: #c0392b;
@@ -38,15 +54,23 @@ h1, h2, h3, h4, h5, h6 {
 #video_output, #download_output {
     border: 1px solid #eb5726;
 }
+.centered-markdown {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    width: 100%;
+}
 """
 
 with gr.Blocks(css=css) as demo:
-    gr.Markdown("# Sickstadium AI")
-    video_url = gr.Textbox(label="Video URL or Filepath", elem_id="video_url")
-    description = gr.Textbox(label="Description of desired clip", elem_id="description")
-    video_output = gr.Video(label="Processed Video", elem_id="video_output")
-    download_output = gr.File(label="Download Processed Video", elem_id="download_output")
-    submit_button = gr.Button("Process Video", elem_id="submit_button")
-    submit_button.click(fn=display_results, inputs=[video_url, description], outputs=[video_output, download_output])
+    with gr.Column():
+        gr.Markdown("# Sickstadium AI", elem_classes="centered-markdown")
+        video_url = gr.Textbox(label="Video URL or Filepath", elem_id="video_url")
+        description = gr.Textbox(label="Description of desired clip", elem_id="description")
+        submit_button = gr.Button("Process Video", elem_id="submit_button")
+        video_output = gr.Video(label="Processed Video", elem_id="video_output")
+        download_output = gr.File(label="Download Processed Video", elem_id="download_output")
+        submit_button.click(fn=display_results, inputs=[video_url, description], outputs=[video_output, download_output])
 
 demo.launch()
