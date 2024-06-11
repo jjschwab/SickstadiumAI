8259902decef2dc962cb4dc24bb9dde2d3530568
Joe Schwab <jschwab21@users.noreply.huggingface.co>
2024-06-11 05:00:22 +0000

    Update app.py

--- app.py
+++ app.py
@@ -119,12 +119,12 @@ body {
     background-image: radial-gradient(#e83f07 1px, transparent 1px);
     background-size: 10px 10px;
     background-repeat: repeat;
-    animation: adjustHue 120s infinite linear;
+    animation: adjustHue 80s infinite linear;
 }
 
 @keyframes adjustHue {
     0% { filter: hue-rotate(0deg); }  
-    50% { filter: hue-rotate(30deg); }  
+    50% { filter: hue-rotate(60deg); }  
     100% { filter: hue-rotate(0deg); }
 }
 #video_url {
